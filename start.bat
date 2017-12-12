f705e165c0a1718bdb1be899525eaec87b5fba86
zonglu <415390588@qq.com>
2017-12-12 18:28:41 +0800

    提交

---
+++ start.bat
@@ -0,0 +1,8 @@
+set DB_SERVER=192.168.0.21
+set MONGO_URL=mongodb://%DB_SERVER%/steedos
+set MONGO_OPLOG_URL=mongodb://%DB_SERVER%/local
+set MULTIPLE_INSTANCES_COLLECTION_NAME=workflow_instances
+set ROOT_URL=http://steedos.ticp.net:3081/
+cls
+meteor run --port 3000 --settings settings.json
+
