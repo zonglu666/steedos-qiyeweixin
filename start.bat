0a8ab20a39ee14372695628e38abf319d61ec2a2
zonglu <415390588@qq.com>
2017-12-26 16:26:08 +0800

    配置文件

--- start.bat
+++ start.bat
@@ -1,6 +1,7 @@
-set DB_SERVER=192.168.0.21
+rem set DB_SERVER=192.168.0.21
+set DB_SERVER=localhost
 set MONGO_URL=mongodb://%DB_SERVER%/steedos
 set MONGO_OPLOG_URL=mongodb://%DB_SERVER%/local
 set MULTIPLE_INSTANCES_COLLECTION_NAME=workflow_instances
-set ROOT_URL=http://steedos.ticp.net:3081/
+set ROOT_URL=http://office.steedos.com:3081/
 meteor run --port 5000 --settings settings.json
